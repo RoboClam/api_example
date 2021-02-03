52be9fd88b14eada04578a4eeceab7e513b5375d
Jacob <jacob.thewrightone@gmail.com>
2021-02-02 21:07:28 -0600

    First python code

--- first_try.py
+++ first_try.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+if(True):
+    print("I am true!")
+
+if(False):
+    print("I am true!")
+
+def mult_two(firstValue,secondValue):
+    print("A value: ")
+    print(firstValue)
+
+    print("B value: ")
+    print(secondValue)
+    return firstValue*secondValue
+
+returnValue = mult_two(10, 10) # this is where the return comes back to
+
+mult_two(returnValue, 5)
+
